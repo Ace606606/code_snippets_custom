e1bb562063f6366f0c0ba141cba903f99ab5dd0c
ace <r.grisha.95@mail.ru>
2026-02-22 09:41:11 +0300

    refactor(spdlog): remove local library search

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -3,18 +3,12 @@ include(FetchContent)
 option(BUILD_SPDLOG "Build external dependencies" ON)
 
 if(BUILD_SPDLOG)
-     message(STATUS "Checking for spdlog...")
-
-     if(spdlog_FOUND)
-          message(STATUS "Found spdlog in system")
-     else()
-          message(STATUS "spdlog not found, fetching from GitHub...")
-          FetchContent_Declare(
-               spdlog
-               GIT_REPOSITORY https://github.com/gabime/spdlog.git
-               GIT_TAG v1.17.0
-          )
-          set(SPDLOG_FMT_EXTERNAL ON CACHE BOOL "" FORCE)
-          FetchContent_MakeAvailable(spdlog)
-     endif()
+     message(STATUS "spdlog fetching from GitHub...")
+     FetchContent_Declare(
+          spdlog
+          GIT_REPOSITORY https://github.com/gabime/spdlog.git
+          GIT_TAG v1.17.0
+     )
+     set(SPDLOG_FMT_EXTERNAL ON CACHE BOOL "" FORCE)
+     FetchContent_MakeAvailable(spdlog)
 endif()
