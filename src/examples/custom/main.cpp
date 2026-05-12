a9d62b955ff1db58ea3283eec6eade81ff8166b9
ace <r.grisha.95@mail.ru>
2026-05-12 20:10:21 +0300

    refactor: add example in custom

--- src/examples/custom/main.cpp
+++ src/examples/custom/main.cpp
@@ -4,13 +4,37 @@
 
 namespace custom
 {
-
      namespace log= csc::utils::logger;
+     void foo()
+     {
+          std::deque< int > deq{ 1, 2, 3 };
 
-}
+          for ( const auto& item : deq  )
+          {
+               log::info( "{}", item );
+          }
+          
+          auto item_front =  deq.front();
+          log::info( "{}", item_front );
+          
+          for ( const auto& item : deq  )
+          {
+               log::info( "{}", item );
+          }
+
+          deq.pop_front();
+
+          for ( const auto& item : deq  )
+          {
+               log::info( "{}", item );
+          }
+     }
+
+} // namespace custom
 
 int main()
 {
      custom::log::info( "{:=^{}}", " Custom ", 50 );
+     custom::foo();
      return 0;
 }
